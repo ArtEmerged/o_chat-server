794743a0ea2448a9cdf2b244890e44649520e69c
artyom_mac <titan4590it@gmail.com>
2024-07-28 18:31:51 +0300

    updated server_grpc layer

--- internal/grpc/chat/server.go
+++ internal/grpc/chat/server.go
@@ -3,13 +3,17 @@ package grpc_chat
 import (
 	"google.golang.org/grpc"
 
+	def "github.com/ArtEmerged/o_chat-server/internal/definitions"
 	desc "github.com/ArtEmerged/o_chat-server/pkg/chat_v1"
 )
 
+var _ desc.ChatV1Server = (*chatServer)(nil)
+
 type chatServer struct {
 	desc.UnimplementedChatV1Server
+	service def.ChatService
 }
 
-func Register(s *grpc.Server) {
-	desc.RegisterChatV1Server(s, &chatServer{})
+func Register(s *grpc.Server, service def.ChatService) {
+	desc.RegisterChatV1Server(s, &chatServer{service: service})
 }
